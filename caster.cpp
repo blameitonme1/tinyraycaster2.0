bdbcbf771af9ead9e84d5fa8c64af019a9a1b19b
bo <yibozhong657@gmail.com>
2024-01-03 21:18:01 +0800

    drawing rays

--- caster.cpp
+++ caster.cpp
@@ -3,14 +3,15 @@
 #include <vector>
 #include <cstdint>
 #include <cassert>
+#include <cmath>
 uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255)
 {
-    // returns a 4 bytes integer, each byte represent a color channel.
+    // 一个4 byte的无符号整数，每一个byte表示一个属性
     return (a << 24) + (b << 16) + (g << 8) + r;
 }
 void unpack_color(const uint32_t &color, uint8_t &r, uint8_t &g, uint8_t &b, uint8_t &a)
 {
-    // from a given color, unpack all the components.
+    // 从给定的一个颜色，将属性抽出来
     r = (color >> 0) & 255;
     g = (color >> 8) & 255;
     b = (color >> 16) & 255;
@@ -52,17 +53,75 @@ int main()
 {
     const size_t win_w = 512;
     const size_t win_h = 512;
-    std::vector<uint32_t> framebuffer(win_w * win_h, 255); // initialising to red
-    for (size_t j = 0; j < win_h; ++j)
-    {
-        for (size_t i = 0; i < win_w; ++i)
+    std::vector<uint32_t> framebuffer(win_w * win_h, 255); // 初始化为红色
+    const size_t map_w = 16;                               // 地图宽度
+    const size_t map_h = 16;                               // 地图高度
+    const char map[] = "0000222222220000"
+                       "1              0"
+                       "1      11111   0"
+                       "1     0        0"
+                       "0     0  1110000"
+                       "0     3        0"
+                       "0   10000      0"
+                       "0   0   11100  0"
+                       "0   0   0      0"
+                       "0   0   1  00000"
+                       "0       1      0"
+                       "2       1      0"
+                       "0       0      0"
+                       "0 0000000      0"
+                       "0              0"
+                       "0002222222200000";    // 游戏地图
+    assert(sizeof(map) == map_h * map_w + 1); // 加上1因为有 '\0'
+    float player_x = 3.456;                   // 玩家x轴坐标
+    float player_y = 2.345;                   // 玩家y轴坐标
+    float player_a = 1.523;                   // 玩家视线和x轴夹角
+    const float sightAngle = M_PI / 3; // 视野角度，三分之一pi
+    for (size_t j = 0; j < win_h; j++)
+    { // 初始化颜色变换
+        for (size_t i = 0; i < win_w; i++)
         {
-            // show color gradient across the image
             uint8_t r = 255 * j / float(win_h);
             uint8_t g = 255 * i / float(win_w);
             uint8_t b = 0;
             framebuffer[i + j * win_w] = pack_color(r, g, b);
         }
     }
+    // 一个地图块在图片里面是一个矩形像素块
+    const size_t rect_w = win_w / map_w;
+    const size_t rect_h = win_h / map_h;
+    // 画出玩家
+    draw_rectangle(framebuffer, win_w, win_h, player_x * rect_w, player_y * rect_h, 5, 5, pack_color(255, 255, 255));
+    for (size_t j = 0; j < map_h; j++)
+    {
+        // 画地图
+        for (size_t i = 0; i < map_w; i++)
+        {
+            if (map[i + j * map_w] == ' ')
+            {
+                continue;
+            }
+            size_t rect_x = i * rect_w;
+            size_t rect_y = j * rect_h;
+            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h,
+                           pack_color(0, 255, 255));
+        }
+    }
+    for(size_t i = 0; i < win_w; ++i){
+        // 画出视野范围, 这里遍历512次是画512条边，这样方便接下来画3d的视野
+        float angle = player_a - sightAngle / 2 + sightAngle * i / (float)win_w;
+        // 画一条玩家的视线图
+        for (float t = 0; t < 20; t += 0.1)
+        {
+            float cx = player_x + t * cos(angle);
+            float cy = player_y + t * sin(angle);
+            if (map[(int)cx + int(cy) * map_w] != ' ')
+                break; // 遇到障碍了
+            size_t ray_x = cx * rect_w;
+            size_t ray_y = cy * rect_h;
+            framebuffer[ray_x + ray_y * win_w] = pack_color(255, 255, 255);
+        }
+    }
     drop_ppm_image("./demo.ppm", framebuffer, win_w, win_h);
+    return 0;
 }
